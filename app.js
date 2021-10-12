d0e8990df1c2fd8a9ecc0f4c6e9d62ee84ea04a1
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-12 13:18:43 +0900

    노마드코더 1 - 2 일차

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-alert('hi');
+// 1. string, number
+const a = 5;
+const b = 2;
+let name = "jihan";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hi "+name);
+
+name = "kojub";
+console.log("what up? "+name);
+
+// 2. boolean
+const amIFat = false;
+console.log(amIFat);
+
+// 3. Arrays
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(daysOfWeek);
+// Get Item from Array
+console.log(daysOfWeek[0]);
+// Add one more dat to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato"];
+toBuy.push("pizza");
+console.log(toBuy[2]);
+
+// 4. Object
+const playerName = "jihan";
+const playerPoints = 121212;
+const playerCute = true;
+const playerFat = "little bit";
+
+const player = {
+    name : "jihan",
+    points : 10, 
+    cute : true,
+    fat : "little bit"
+}
+
+console.log(player.name);
+console.log(player["points"]);
+player.lastName = "kim";
+console.log(player);
