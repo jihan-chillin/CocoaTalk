c81d8b8233b9a00e46a5c70c63556691386a3470
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-13 13:48:01 +0900

    바닐라 JS 3일차 returns

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-// function 
-function sayHello(name, age){
-    console.log("Hello my nam is "+name+" and i'm "+age);
-}
-
-sayHello("jihan", 20);
-sayHello("starbucks", 10);
-sayHello("americano", 30);
-
-function plus(firstNum,secondNum){
-    console.log(firstNum + secondNum);
-}
-
-function divide(a, b){
-    console.log(a/b);
-}
-
-plus(8, 60);
-divide(60, 12);
-
-// object + function
-const player = {
-    name : "jihan", 
-    sayHello : function(otherPersonsName){
-        console.log("hello! "+otherPersonsName);
+// returns
+const calculator = {
+    plus : function(a,b){
+        return(a+b);
+    },
+    minus : function(a,b){
+        return(a-b);
+    },
+    multi : function(a,b){
+        return(a*b);
+    },
+    divide : function(a,b){
+        return(a/b);
+    },
+    power : function(a,b){
+        return(a**b);
     }
 }
 
-console.log(player.name);
-player.sayHello("buya");
+calculator.plus(10,5);
+calculator.minus(10,2);
+calculator.multi(10,2);
+calculator.divide(10,2);
+calculator.power(10,2);
+
+const plusResult = calculator.plus(2,3);
+console.log("더하기 값 : "+plusResult);
+
+
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2;
+}
+
+const krAge = calculateKrAge(age);
+console.log(krAge);
