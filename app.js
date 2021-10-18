1fd7a8dd920b960dd311fd0ec9f00caa79ec37c6
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-18 14:54:45 +0900

    Css in javascript

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 // Events
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick(){
+//     const currentColor = h1.style.color;
+//     let newColor ;
+//     if(currentColor === "blue"){
+//         newColor = "tomato";
+//     }
+//     else {
+//         newColor = "blue";
+//     }
+//     h1.style.color = newColor;
+// }
+
+// function handleMouseEnter(){
+//     console.log("mouse is here");
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+
+
+// Css in javscript 
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick() {
+//     const clickedClass = "clicked"
+//     if(h1.classList.contains(clickedClass)){
+//         h1.classList.remove(clickedClass);
+//     }else{
+//         h1.classList.add(clickedClass);
+//     }
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+
+// Css in javascript part 2 => toggle
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor ;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    }
-    else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
-}
-
-function handleMouseEnter(){
-    console.log("mouse is here");
+function handleTitleClick() {
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
