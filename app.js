fa282e830a151a2ee08c13ee41061933ad8bff30
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2021-10-21 20:31:27 +0900

    event.preventDefault() : default 이벤트 방지

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 // const loginButton = document.querySelector("#login-form button");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     // ▼ default behavior를 막는 것 (submit으로 인해 새로고침 되는 이벤트)
     event.preventDefault();
@@ -14,4 +16,13 @@ function onLoginSubmit(event){
     // }
 }
 
+function handleLinkClick(event){
+    // 블로그로 넘어가는 거 막기 
+    event.preventDefault();
+    console.dir(event);
+    alert("clicked");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit)
+link.addEventListener("click", handleLinkClick);
+
